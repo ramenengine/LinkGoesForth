ebb4b26f8f5dc25150143b3f37a71e634e8008dc
Unknown <roger.levy@gmail.com>
2018-07-20 22:18:12 -0400

    add engine.f

--- src/main.f
+++ src/main.f
@@ -1,126 +1,31 @@
     include afkit/ans/section.f
     warning off
 
+\ ---------------------------------------------------------------------------------------------------------
 [section] preamble
 empty
-    #2 #0 #0 include ramen/brick.f
-    65536 2 * constant #MAXTILES
-    require ramen/tiled/tiled.f
-    require ramen/lib/array2d.f
-
-[section] variables
-    0 value bgbank
-    0 value sprbank
-    0 value tinter
-
-    \ tile size
-    16 constant tw
-    16 constant th
+    include src/engine.f
     
-    \ tile buffers
-    tilebuf constant tilebuf0
-    1024 1024 buffer2d: tilebuf1
-
-    struct layer
-        layer 0 svar bg.scrollx
-        layer 0 svar bg.scrolly
-
-    objlist stage
-        stage 256 pool: objects0
-        stage 256 pool: objects1
-        stage 256 pool: objects2
-
-    \ the layers are orphan objects
-    create spr0 object  en on
-    create bg0 object   en on
-    create spr1 object  en on
-    create bg1 object   en on
-    create spr2 object  en on
-
-    \ add object priority:
-        var priority
-
-    \ extend objects: transform info (used by layers too)
-        var sx  var sy  var ang
-        color sizeof field tint
-
-    \ extend objects: bg layer stuff
-        var scrollx
-        var scrolly
-        var tbi       \ tile base index
-
-    \ extending the renderer; draw stuff either under or over the game
-    defer background  :is background ;
-    defer overlay     :is overlay ;
-
-    \ window rectangle
-    create window 0 , 32 , displayw , displayh 32 - ,
-
-    \ screen coordinates
-    create coords  0 , 0 ,
-
-[section] init
-
-\ load sprite data
-s" sprites.dat" file-exists [if]
-    s" sprites.dat" sprites /sprites @file
-[then]
-s" sprites.f" file-exists [if]
-    s" sprites.f" included
-[then]
-
-: loadgfx
-    s" data/bg.png" findfile loadbmp to bgbank
-    s" data/spr.png" findfile loadbmp to sprbank
-    bgbank tw th 1 maketiles >r
-    sprbank tw th r> maketiles drop
-;
-: init
-    loadgfx
-    1024 for 1024 for  $10 rnd i j tilebuf0 loc !  loop loop
-    nativewh *bmp to tinter
-; init
-
-: reinit  -tiles loadgfx ;
-
-[section] layers
-: transformed ;
-
-: drawsprlayer  ( list -- )
-    en @ 0= if drop exit then
-    transformed  drawzsorted ;
-
-: windowmap  ( array2d -- )
-    window 4@ clip>
-        window 2@ +at
-        scrollx 2@ tw th scroll
-            third loc  swap pitch@  tilemap ;
-: drawbglayer
-    en @ 0= if drop exit then
-    transformed windowmap
-;
-: layers
-    {
-    objects0 spr0 as drawsprlayer
-    tilebuf0 bg0 as drawbglayer
-    objects1 spr1 as drawsprlayer
-    tilebuf1 bg1 as drawbglayer
-    objects2 spr2 as drawsprlayer
-    }
-;
-
-[section] go
-: think  stage each> act ;
-: physics  stage each> vx x v+  y @ zdepth ! ;
-: /step  step> think stage multi ;
-: overworld show> black backdrop background layers overlay ;
-: go /step overworld ; go
+\ stuff and things
+\   ability to pause gameplay during status screen
+\   character
+\   game data
+\   save data
 
 
+\ ---------------------------------------------------------------------------------------------------------
 [section] hud
-:is overlay  0 0 at  displayw 32 black rectf ;
 
 
+:is overlay
+    0 0 at  displayw 32 black rectf
+    \ show hearts
+    \ show current item
+    \ actually maybe could also draw the status screen here too just offscreen (we scroll up to see it)
+;
+
+
+\ ---------------------------------------------------------------------------------------------------------
 [section] test
 
 : north  coords y@ -exit  -1 coords y+!  4 13 * for  -4 bg0 's scrolly +!  pause  loop ;
@@ -138,13 +43,10 @@ s" sprites.f" file-exists [if]
             <down> kpressed? if  south  then
             <left> kpressed? if  west  then
             <right> kpressed? if  east  then
-            <enter> kpressed? if  0 ['] rld later  then
+            <enter> kpressed? if  0 [']  rld later  then
             pause
         again
 ;
-
-
-
 : *thing
     objects1 one
 ;
