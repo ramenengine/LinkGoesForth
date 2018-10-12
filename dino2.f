89abd75d1fe9e55cc720b4236cabf782da37ec42
Unknown <roger.levy@gmail.com>
2018-10-12 17:12:09 -0400

    dino
    Create dino2.f
    Update dino2.f
    Update dino2.f
    Create stuff.f
    Update dino2.f

---
+++ dino2.f
@@ -0,0 +1,4 @@
+include stuff.f  \ refreshes assets
+
+: spin  act>  1 ang +! ;
+: *dino  dinobaby.png myimage  *sprite  10 10 sx 2!  spin ;
---
+++ stuff.f
@@ -0,0 +1,2 @@
+: cimg!  img !  img @ imagewh 2halve cx 2! ;
+: *sprite  objects one  cimg!  draw> sprite+ ;
